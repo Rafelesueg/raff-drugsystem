c9a0a6064f7939a3be62aa831c1c1e405950f866
Rafelesueg <68927180+Rafelesueg@users.noreply.github.com>
2023-10-06 13:15:59 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,21 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-
-shared_scripts {
-    '@es_extended/imports.lua',
-	'@ox_lib/init.lua',
-	-- 'modules/init.lua'
-}
-
-client_scripts {
-    '@PolyZone/client.lua',
+client_scripts {,
     'config.lua',
     'client/*.lua',
 }
 
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
+server_scripts {,
     'config.lua',
     'server/*.lua',
 }
